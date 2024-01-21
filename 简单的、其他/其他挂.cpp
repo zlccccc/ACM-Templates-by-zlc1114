2a313073a78b8db568e6c0f4115bbced5d96c0f3
zlc1114 <zlc1114@buaa.edu.cn>
2024-01-22 00:51:27 +0800

    更新了一下板子常数

--- 简单的、其他/其他挂.cpp
+++ 简单的、其他/其他挂.cpp
@@ -1,4 +1,4 @@
-扩栈挂
+// 扩栈挂
 #ifdef OPENSTACK
     int size = 256 << 20; // 256MB
     char *p = (char*)malloc(size) + size;
@@ -8,14 +8,13 @@
         __asm__("movl %0, %%esp\n" :: "r"(p));
     #endif
 #endif
-    注意最后加exit(0);
+// 注意最后加exit(0);
 
-玄学挂
+// 玄学挂
 #pragma comment(linker, "/stack:200000000")
 #pragma GCC optimize("Ofast,no-stack-protector")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
 
-然后加上并行计算(计组)
-
+// 然后加上并行计算(计组)
 #pragma GCC optimize("Ofast,no-stack-protector")
 #pragma GCC target("avx") 
